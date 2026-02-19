0d8960c5b01441577bfedfd418b1ab322390fae0
christian <christiannemeno80@gmail.com>
2026-02-19 17:19:16 +0800

    token and tokentype

---
+++ Lexor/Token.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexor
+{
+    internal class Token
+    {
+        public TokenType Type { get; }
+        public String? Lexeme { get; }
+        public Object? Literal { get; }
+        public int Line { get; }
+
+        public Token(TokenType type, String? lexeme, Object? literal, int line)
+        {
+            Type = type;
+            Lexeme = lexeme;
+            Literal = literal;
+            Line = line;
+        }
+
+        public override String ToString() => $"{Type} {Lexeme} {Literal ?? "null"}";
+
+
+
+
+    }
+}
---
+++ Lexor/TokenType.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexor
+{
+    internal enum TokenType
+    {
+        // Parenthesis (, )
+        LEFT_PAREN,
+        RIGHT_PAREN,
+        // * , / , %
+        STAR, SLASH, MOD,
+        // +, -
+        PLUS, MINUS,
+        // > , < , <=, >= , ==, <>
+        GREATER, LESS,
+        GREATER_EQUAL, LESS_EQUAL,
+        DOUBLE_EQUAL, NOT_EQUAL,
+
+        // =    
+        EQUAL,
+
+
+        // logical ops
+        AND, OR, NOT, TRUE, FALSE,
+
+        //.
+        DOT,
+
+        // data types
+        INT, CHAR, BOOL, FLOAT,
+
+        // literals
+        IDENTIFIER, CHAR_LITERAL, STRING_LITERAL, INT_LITERAL, FLOAT_LITERAL,
+
+        //symbols
+        COMMA, AMPERSAND, DOLLAR,
+
+        // reserved keywords
+        SCRIPT_AREA, START_SCRIPT, END_SCRIPT,
+        DECLARE,
+
+        // the '=' button
+        ASSIGN,
+
+        // control flow
+        IF, START_IF, END_IF,
+        ELSE, ELSE_IF,
+
+        //loop
+        FOR, START_FOR, END_FOR,
+        REPEAT_WHEN, START_REPEAT, END_REPEAT,
+
+
+        EOF,
+    }
+}
