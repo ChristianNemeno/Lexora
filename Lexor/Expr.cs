38afa8e98562f6d3e251867370d5566e6c12f38b
christian <christiannemeno80@gmail.com>
2026-02-24 22:24:26 +0800

    Expr

--- Lexor/Expr.cs
+++ Lexor/Expr.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Lexor.Expr.Assign;
 
 namespace Lexor
 {
@@ -11,52 +12,118 @@ namespace Lexor
         
         public interface IVisitor<R>
         {
-
+            R VisitAssignExpr(Assign expr);
+            R VisitBinaryExpr(Binary expr);
+            R VisitGroupingExpr(Grouping expr);
+            R VisitLiteralExpr(Literal expr);
+            R VisitUnaryExpr(Unary expr);
+            R VisitVariableExpr(Variable expr);
         }
 
-        public abstract R Accept <R>(IVisitor<R> visitor);
         public class Assign : Expr
         {
-            private Token Name;
-            private Expr value;
+            public readonly Token Name;
+            public readonly Expr Value;
 
             public Assign(Token name, Expr value)
             {
                 Name = name;
-                this.value = value;
+                Value = value;
             }
 
             public override R Accept<R>(IVisitor<R> visitor)
             {
-                throw new NotImplementedException();
+                return visitor.VisitAssignExpr(this);
             }
+        }
+   
+        public class Binary : Expr
+        {
+            public readonly Expr Left;
+            public readonly Token Operator;
+            public readonly Expr Right;
 
-
-            public class Binary : Expr
+            public Binary(Expr left, Token op, Expr right)
             {
-                private Expr Left;
-                private Token Operator;
-                private Expr Right;
-
-                public Binary(Expr left, Token op, Expr right)
-                {
-                    Left = left;
-                    Operator = op;
-                    Right = right;
-                }
-
-                public override R Accept <R> (IVisitor<R> visitor)
-                {
-                    throw new NotImplementedException();
-                }
-
+                Left = left;
+                Operator = op;
+                Right = right;
             }
-            
 
-
-
-
+            public override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitBinaryExpr(this);
+            }
         }
 
+        public class Grouping : Expr
+        {
+            public readonly Expr Expression;
+
+            public Grouping(Expr expression)
+            {
+                Expression = expression;
+            }
+
+            public override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitGroupingExpr(this);
+            }
+        }
+
+       
+        public class Literal : Expr
+        {
+            public readonly object Value;
+
+            public Literal(object value)
+            {
+                Value = value;
+            }
+
+            public override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitLiteralExpr(this);
+            }
+        }
+
+        public class Unary : Expr
+        {
+            public readonly Token Operator;
+            public readonly Expr Right;
+
+            public Unary(Token op, Expr right)
+            {
+                Operator = op;
+                Right = right;
+            }
+
+            public override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitUnaryExpr(this);
+            }
+        }
+
+        public class Variable : Expr
+        {
+            public readonly Token Name;
+
+            public Variable(Token name)
+            {
+                Name = name;
+            }
+
+            public override R Accept<R>(IVisitor<R> visitor)
+            {
+                return visitor.VisitVariableExpr(this);
+            }
+        }
+
+        public abstract R Accept<R>(IVisitor<R> visitor);
     }
+
+
+
+
+
 }
