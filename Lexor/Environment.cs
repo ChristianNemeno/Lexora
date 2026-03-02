65b8871af7fcf9d27d9705b8c00f2111cb92b75c
christian <christiannemeno80@gmail.com>
2026-03-02 15:03:04 +0800

    the environment class where it holds the vlaues of declared stuff

---
+++ Lexor/Environment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexor
+{
+    class Environment
+    {
+        Dictionary<string, object?> values = new Dictionary<string, object?>();
+
+        public void Define(string name, object? value)
+        {
+            values[name] = value;
+        }
+
+        public object? Get(Token name) { 
+            if(values.ContainsKey(name.Lexeme!)) return values[name.Lexeme!];
+
+            throw new RuntimeError(name, $"Undefined variable '{name.Lexeme}'.");
+        }
+
+        public void Assign(Token name, object? value) { 
+            if(values.ContainsKey(name.Lexeme!))
+            {
+                values[name.Lexeme!] = value;
+                return;
+            }
+            throw new RuntimeError(name, $"Undefined variable '{name.Lexeme}'.");
+        }
+
+
+    }
+}
